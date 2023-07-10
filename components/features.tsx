7ad6cad852c695c23b28773436f75959208fc492
CleshanW <cwar0007@student.monash.edu>
2023-07-10 14:56:03 +1000

    added logo and changes

--- components/features.tsx
+++ components/features.tsx
@@ -6,8 +6,8 @@ export default function Features() {
 
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
-            <h2 className="h2 mb-4">The majority our customers do not understand their workflows.</h2>
-            <p className="text-xl text-gray-400">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <h2 className="h2 mb-4">AI Assistant: Your Sidekick for Effortless Coding!</h2>
+            <p className="text-xl text-gray-400">Experience effortless coding with our AI Assistant, your sidekick powered by advanced machine learning, providing intelligent code predictions, automated tasks, and context-aware recommendations in Visual Studio Code.</p>
           </div>
 
           {/* Items */}
--- components/hero.tsx
+++ components/hero.tsx
@@ -1,5 +1,7 @@
 import VideoThumb from '@/public/images/hero-image-01.jpg'
 import ModalVideo from '@/components/modal-video'
+import PyCoder_Icon from '@/public/images/Icon.png'
+import Image from 'next/image'
 
 export default function Hero() {
   return (
@@ -20,20 +22,20 @@ export default function Hero() {
         </div>
 
         {/* Hero content */}
-        <div className="relative pt-32 pb-10 md:pt-40 md:pb-16">
+        <div className="relative pt-32 pb-20 md:pt-15 md:pb-16">
 
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
-            <h1 className="h1 mb-4" data-aos="fade-up">Landing template for startups</h1>
-            <p className="text-xl text-gray-400 mb-8" data-aos="fade-up" data-aos-delay="200">Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.</p>
+          <Image className="h1 mb-4 mx-auto text-center sm:w-auto sm:mb-0" src={PyCoder_Icon}  data-aos="fade-up" alt="Testimonial 01" />
+            <p className="text-xl text-gray-400 mb-8" data-aos="fade-up" data-aos-delay="200">Coding Made Simplier</p>
             <div className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center">
               <div data-aos="fade-up" data-aos-delay="400">
-                <a className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="#0">Start free trial</a>
+                <a className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="#0">Download Now</a>
               </div>
               <div data-aos="fade-up" data-aos-delay="600">
                 <a className="btn text-white bg-gray-700 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="#0">Learn more</a>
               </div>
-            </div>
+            </div> 
           </div>
 
           <ModalVideo
