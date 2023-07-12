5bda3c57a5b82b1e0a56a16467991fa853b3b65e
Matthew <mhee0019@student.monash.edu>
2023-07-12 16:56:09 +1000

    Added PyCoder feature descriptions

--- components/zigzag.tsx
+++ components/zigzag.tsx
@@ -12,9 +12,9 @@ export default function Zigzag() {
 
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
-            <div className="inline-flex text-sm font-semibold py-1 px-3 m-2 text-green-600 bg-green-200 rounded-full mb-4">Reach goals that matter</div>
-            <h1 className="h2 mb-4">One product, unlimited solutions</h1>
-            <p className="text-xl text-gray-400">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit laborum — semper quis lectus nulla.</p>
+            <div className="inline-flex text-sm font-semibold py-1 px-3 m-2 text-green-600 bg-green-200 rounded-full mb-4">Focus on solving bigger problems</div>
+            <h1 className="h2 mb-4">Code less, Deliver more</h1>
+            <p className="text-xl text-gray-400"> PyCoder handles mundane tasks, while you deliver innovative features.</p>
           </div>
 
           {/* Items */}
@@ -29,10 +29,10 @@ export default function Zigzag() {
               {/* Content */}
               <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6" data-aos="fade-right">
                 <div className="md:pr-4 lg:pr-12 xl:pr-16">
-                  <div className="font-architects-daughter text-xl text-purple-600 mb-2">More speed. Less spend</div>
-                  <h3 className="h3 mb-3">Keep projects on schedule</h3>
-                  <p className="text-xl text-gray-400 mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                  <ul className="text-lg text-gray-400 -mb-2">
+                  <div className="font-architects-daughter text-xl text-purple-600 mb-2">Focus on what matters</div>
+                  <h3 className="h3 mb-3">Accelerate delivery</h3>
+                  <p className="text-xl text-gray-400 mb-4">PyCoder speeds up software delivery processes by eliminating repetitive coding tasks, boosting productivity and efficiency.</p>
+                  {/* <ul className="text-lg text-gray-400 -mb-2">
                     <li className="flex items-center mb-2">
                       <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
@@ -51,7 +51,7 @@ export default function Zigzag() {
                       </svg>
                       <span>Amet consectetur adipiscing elit</span>
                     </li>
-                  </ul>
+                  </ul> */}
                 </div>
               </div>
             </div>
@@ -65,10 +65,10 @@ export default function Zigzag() {
               {/* Content */}
               <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6" data-aos="fade-left">
                 <div className="md:pl-4 lg:pl-12 xl:pl-16">
-                  <div className="font-architects-daughter text-xl text-purple-600 mb-2">More speed. Less spend</div>
-                  <h3 className="h3 mb-3">Keep projects on schedule</h3>
-                  <p className="text-xl text-gray-400 mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                  <ul className="text-lg text-gray-400 -mb-2">
+                  <div className="font-architects-daughter text-xl text-purple-600 mb-2">Personalised suggestions</div>
+                  <h3 className="h3 mb-3">Get suggestions, just for you</h3>
+                  <p className="text-xl text-gray-400 mb-4">PyCoder shows suggestions based on code written by you in real time. Quickly cycle through multiple suggestions and decide to accept or reject.</p>
+                  {/* <ul className="text-lg text-gray-400 -mb-2">
                     <li className="flex items-center mb-2">
                       <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
@@ -87,7 +87,7 @@ export default function Zigzag() {
                       </svg>
                       <span>Amet consectetur adipiscing elit</span>
                     </li>
-                  </ul>
+                  </ul> */}
                 </div>
               </div>
             </div>
@@ -101,10 +101,10 @@ export default function Zigzag() {
               {/* Content */}
               <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6" data-aos="fade-right">
                 <div className="md:pr-4 lg:pr-12 xl:pr-16">
-                  <div className="font-architects-daughter text-xl text-purple-600 mb-2">More speed. Less spend</div>
-                  <h3 className="h3 mb-3">Keep projects on schedule</h3>
-                  <p className="text-xl text-gray-400 mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                  <ul className="text-lg text-gray-400 -mb-2">
+                  <div className="font-architects-daughter text-xl text-purple-600 mb-2">Simple installation</div>
+                  <h3 className="h3 mb-3">Easy integration</h3>
+                  <p className="text-xl text-gray-400 mb-4">Pycoder integrates directly into your favourite Visual Studio Code editor. Experience PyCoder today with a smooth and hassle-free setup.</p>
+                  {/* <ul className="text-lg text-gray-400 -mb-2">
                     <li className="flex items-center mb-2">
                       <svg className="w-3 h-3 fill-current text-green-500 mr-2 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
@@ -123,7 +123,7 @@ export default function Zigzag() {
                       </svg>
                       <span>Amet consectetur adipiscing elit</span>
                     </li>
-                  </ul>
+                  </ul> */}
                 </div>
               </div>
             </div>
