e79d28a8c9d6955fc9af01cd0e8b4c1c988c5b7b
Matthew <mhee0019@student.monash.edu>
2023-08-04 14:27:42 +1000

    Added cards for supervisors

--- components/testimonials.tsx
+++ components/testimonials.tsx
@@ -3,6 +3,9 @@ import Image from 'next/image'
 import clesh from '@/public/images/clesh.jpg'
 import matthew from '@/public/images/Matthew.jpg'
 import christian from '@/public/images/Christian.jpg'
+import kla from '@/public/images/Kla.jpg'
+import wannita from '@/public/images/Wannita.jpg'
+
 
 export default function Testimonials() {
   return (
@@ -64,6 +67,38 @@ export default function Testimonials() {
               <blockquote className="text-lg text-gray-400 grow">I have learnt some of the usefulness and limitations of AI and how this technology can be incorporated into my projects. It has been a great experience to leverage the existing PyCoder model to create a code completion tool to help software engineers.</blockquote>
               <div className="text-gray-700 font-medium mt-6 pt-5 border-t border-gray-700">
                 <cite className="text-gray-200 not-italic">Christian Yaacoub</cite> - <a className="text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out" href="https://www.linkedin.com/in/christian-yaacoub-a530a9246/">LinkedIn</a>
+              </div>
+            </div>
+
+            {/* 4th testimonial */}
+            <div className="flex flex-col h-full p-6 bg-gray-800" data-aos="fade-up" data-aos-delay="400">
+              <div>
+                <div className="relative inline-flex flex-col mb-4">
+                  <Image className="rounded-full"  src={kla} width={48} height={48} alt="Testimonial 03" />
+                  <svg className="absolute top-0 right-0 -mr-3 w-6 h-5 fill-current text-purple-600" viewBox="0 0 24 20" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M0 13.517c0-2.346.611-4.774 1.833-7.283C3.056 3.726 4.733 1.648 6.865 0L11 2.696C9.726 4.393 8.777 6.109 8.152 7.844c-.624 1.735-.936 3.589-.936 5.56v4.644H0v-4.531zm13 0c0-2.346.611-4.774 1.833-7.283 1.223-2.508 2.9-4.586 5.032-6.234L24 2.696c-1.274 1.697-2.223 3.413-2.848 5.148-.624 1.735-.936 3.589-.936 5.56v4.644H13v-4.531z" />
+                  </svg>
+                </div>
+              </div>
+              <blockquote className="text-lg text-gray-400 grow">We want to make software engineers more smart and productive, and spend more time on their creativity rather than repetitive coding tasks.</blockquote>
+              <div className="text-gray-700 font-medium mt-6 pt-5 border-t border-gray-700">
+                <cite className="text-gray-200 not-italic">Dr. Kla Tantithamthavorn</cite> - <a className="text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out" href="https://www.linkedin.com/in/kla-tantithamthavorn-49ab0176/">LinkedIn</a>
+              </div>
+            </div>
+
+            {/* 5th testimonial */}
+            <div className="flex flex-col h-full p-6 bg-gray-800" data-aos="fade-up" data-aos-delay="400">
+              <div>
+                <div className="relative inline-flex flex-col mb-4">
+                  <Image className="rounded-full"  src={wannita} width={48} height={48} alt="Testimonial 03" />
+                  <svg className="absolute top-0 right-0 -mr-3 w-6 h-5 fill-current text-purple-600" viewBox="0 0 24 20" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M0 13.517c0-2.346.611-4.774 1.833-7.283C3.056 3.726 4.733 1.648 6.865 0L11 2.696C9.726 4.393 8.777 6.109 8.152 7.844c-.624 1.735-.936 3.589-.936 5.56v4.644H0v-4.531zm13 0c0-2.346.611-4.774 1.833-7.283 1.223-2.508 2.9-4.586 5.032-6.234L24 2.696c-1.274 1.697-2.223 3.413-2.848 5.148-.624 1.735-.936 3.589-.936 5.56v4.644H13v-4.531z" />
+                  </svg>
+                </div>
+              </div>
+              <blockquote className="text-lg text-gray-400 grow">We want to make software engineers more smart and productive, and spend more time on their creativity rather than repetitive coding tasks.</blockquote>
+              <div className="text-gray-700 font-medium mt-6 pt-5 border-t border-gray-700">
+                <cite className="text-gray-200 not-italic">Wannita Takerngsaksiri</cite> - <a className="text-purple-600 hover:text-gray-200 transition duration-150 ease-in-out" href="https://www.linkedin.com/in/wannita-takerngsaksiri/">LinkedIn</a>
               </div>
             </div>
 
